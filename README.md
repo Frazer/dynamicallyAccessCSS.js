7a1e9e09d5538eb6be0bdeaa51bf4a9825317639
Frazer Kirkman <frazer@frazerk.net>
2017-08-08 12:32:41 +0200

    polished functionality, updated readme, and fixed bug for old versions of safari

--- README.md
+++ README.md
@@ -7,8 +7,22 @@ it is as simple as
     
     ...
     
-    onclick="getCSSRule('classYouWantToChange').style.colorOrSizeOrWhatever = '3px'"
+    onclick="getCSSRule('selectorYouWantToChange').style.colorOrSizeOrWhatever = '3px'"
+
+    or
+
+    onclick="getCSSRule('selectorYouWantToChange').style.setProperty ("width", "10px", "important")""
     
+
+
+    if you want to get an array of all of the rules that reference your selector, you use:
+    getCSSRule('selectorYouWantToChange', true)
+
+    to change the last rule
+	var myRules  = getCSSRule('selectorYouWantToChange', true);
+	var lastRule = myRules[myRules.length-1]
+    lastRule.style.colorOrSizeOrWhatever = 'something'
+
     
 this was inspired here:
 http://stackoverflow.com/questions/730048/how-to-change-remove-css-classes-definitions-at-runtime
--- dynamicallyAccessCSS.js
+++ dynamicallyAccessCSS.js
@@ -1,3 +1,12 @@
+/**
+ * dynamicallyAccessCSS 
+ *
+ * @link    https://github.com/Frazer/dynamicallyAccessCSS.js
+ * @license MIT
+ *          
+ * @author  Frazer Kirkman
+ * @published 2017
+ */
 
 var returnStyleSheetRules = (function (){  
 	if(!document.styleSheets[0]){
@@ -17,24 +26,47 @@ var returnStyleSheetRules = (function (){
 	}
 })();
 
-function getCSSRule(search) {
-	rule = [].map.call(document.styleSheets, function(item) {
+function getCSSRule(search, returnArray) {
+
+	let styleSheets = [].map.call(document.styleSheets, function(item) {
 	  return [].slice.call(returnStyleSheetRules(item));
-	}).reduce(function(a, b) {
-	  return b.concat(a);
-	}).filter(function(rule) {
-	  return rule.selectorText.lastIndexOf(search) === rule.selectorText.length - search.length;
-	})[0];
+	});
+
+	let rule = null;
+	let rules = [];
+	styleSheets.forEach(function(thisSheet){
+	  let findTheRule = thisSheet.filter(function(rule) {
+	    if(rule.selectorText){
+	    	return rule.selectorText.lastIndexOf(search)===0  && search.length===rule.selectorText.length;	
+	    }else return false;
+	  });
+
+	  if(findTheRule.length){
+			rules = rules.concat(findTheRule);
+			rule = findTheRule[findTheRule.length-1];    //findTheRule will contain all rules that reference the selector. findTheRule[findTheRule.length-1] contains the last rule.
+	  }
+	});
 	if (rule){
-	   return rule;
+		if(returnArray){
+			return rules;
+		}else{
+			return rule;
+		}
 	}else{
 		let sheet = document.styleSheets[0];   //if the rule we are looking for doesn't exist, we create it
-		if("insertRule" in sheet) {
-			sheet.insertRule("."+search + "{  }");
+        var pos = sheet.cssRules.length;
+        if("insertRule" in sheet) {
+                sheet.insertRule(search + "{  }",pos);
+        }
+        else if("addRule" in sheet) {
+                sheet.addRule(search, "",pos);
+        }
+		if(returnArray){
+			return returnStyleSheetRules(document.styleSheets[0]);
+		}else{
+			return returnStyleSheetRules(document.styleSheets[0])[pos];
 		}
-		else if("addRule" in sheet) {
-			sheet.addRule(search, "");
-		}
-		return returnStyleSheetRules(document.styleSheets[0])[0];
 	}
 }
+
+
